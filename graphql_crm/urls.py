8985bf42d7b1a9f7afac464a546173a67318eaec
TAREK <tareksayedmokhtar@yahoo.com>
2025-12-11 01:42:04 +0200

    Create urls.py

---
+++ graphql_crm/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "graphql",
+        csrf_exempt(
+            GraphQLView.as_view(
+                graphiql=True,  # Enable GraphiQL for interactive exploration
+            )
+        ),
+        name="graphql-endpoint",
+    ),
+]
