5a2070e56327e73edb4fd14e36259f5d1de6d8da
TAREK <tareksayedmokhtar@yahoo.com>
2025-12-11 17:46:45 +0200

    Update and rename wsgi.py to wsgi.py

--- alx_backend_graphql/wsgi.py
+++ alx_backend_graphql/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphql_crm.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_backend_graphql.settings")
 
 application = get_wsgi_application()
